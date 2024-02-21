82102670c74e5877a159592422f6c526603fdc4a
ウスマーン <82204694+uthmaanb@users.noreply.github.com>
2024-02-21 09:10:55 +0200

    first commit

---
+++ src/App.vue
@@ -0,0 +1,11 @@
+<script setup lang="ts">
+import AppHeader from './components/AppHeader.vue'
+import TodoList from './components/TodoList.vue'
+</script>
+
+<template>
+  <AppHeader />
+  <TodoList />
+</template>
+
+<style scoped></style>
---
+++ src/components/TodoList.vue
@@ -0,0 +1,88 @@
+<script lang="ts" setup>
+import { ref, onMounted, computed } from 'vue'
+import type { Ref } from 'vue'
+import ListItem from './ListItem.vue'
+
+type Item = {
+  title: string
+  checked?: boolean
+}
+
+const storageItems: Ref<Item[]> = ref([])
+
+const setToStorage = (items: Item[]): void => {
+  localStorage.setItem('list-items', JSON.stringify(items))
+}
+
+const getFromStorage = (): Item[] | [] => {
+  const stored = localStorage.getItem('list-items')
+  if (stored) {
+    return JSON.parse(stored)
+  }
+  return []
+}
+
+const initListItems = (): void => {
+  if (storageItems.value?.length === 0) {
+    const listItems = [
+      { title: 'Make a Todo list app', checked: true },
+      { title: 'Predict the weather', checked: false },
+      { title: 'Play some tunes', checked: false },
+      { title: 'Let\`s get cooking', checked: false },
+      { title: 'Pump some iron', checked: false },
+      { title: 'Track my expenses', checked: false },
+      { title: 'Organise a game night', checked: false },
+      { title: 'Learn a new language', checked: false },
+      { title: 'Publish my work', checked: false }
+    ]
+    setToStorage(listItems)
+    storageItems.value = listItems
+  }
+}
+
+const sortedList = computed(() =>
+  [...storageItems.value].sort((a, b) => (a.checked ? 1 : 0) - (b.checked ? 1 : 0))
+)
+
+const updateItem = (item: Item): void => {
+  const updatedItem = findItemInList(item)
+  if (updatedItem) {
+    console.log('Updating item:', item)
+    toggleItemChecked(updatedItem)
+    setToStorage(storageItems.value)
+  }
+}
+
+const findItemInList = (item: Item): Item | undefined => {
+  return storageItems.value.find((itemInList: Item) => itemInList.title === item.title)
+}
+
+const toggleItemChecked = (item: Item): void => {
+  item.checked = !item.checked
+}
+
+onMounted(() => {
+  initListItems()
+  storageItems.value = getFromStorage()
+})
+</script>
+
+<template>
+  <ul>
+    <li :key="key" v-for="(item, key) in sortedList">
+      <ListItem :is-checked="item.checked" v-on:click.prevent="updateItem(item)">{{
+        item.title
+      }}</ListItem>
+      <!-- shorthand for v-on:click is @click -->
+    </li>
+  </ul>
+</template>
+
+<style scoped>
+ul {
+  list-style: none;
+}
+li {
+  margin: 0.4rem 0;
+}
+</style>
